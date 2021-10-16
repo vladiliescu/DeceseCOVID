4e923396101fc2c2cf635469151747d2c8c5b875
Vlad Iliescu <vlad.iliescu@gmail.com>
2021-10-16 13:29:34 +0300

    Final layout, fix colors to be more colorblind-friendly

--- render.py
+++ render.py
@@ -16,7 +16,7 @@ def svg_settings():
     but it doesn't work when using template arguments for the calculations
     :return:
     """
-    max_width = 1280
+    max_width = 1024
 
     sprite_height = 60
     sprite_width = 25
@@ -25,9 +25,9 @@ def svg_settings():
     sprite_distance_y = 5
 
     x_start = 25
-    y_start = 200
+    y_start = 220
 
-    y_bottom_padding = 200
+    y_bottom_padding = 120
     x_right_padding = 20
 
     sprites_per_row = math.floor((max_width - x_start - x_right_padding) / (sprite_width + sprite_distance_x))
@@ -37,13 +37,14 @@ def svg_settings():
     max_height = y_start + sprite_rows * row_height + y_bottom_padding
 
     return {
-        'max_width': max_width,
+        'max_width': max_width, 'max_height': max_height,
         'sprite_height': sprite_height, 'sprite_width': sprite_width,
-        'sprite_distance_x': sprite_distance_x, 'sprite_distance_y': sprite_distance_y,
+        'sprite_distance_x': sprite_distance_x,  'sprite_distance_y': sprite_distance_y,
         'x_start': x_start, 'y_start': y_start,
         'y_bottom_padding': y_bottom_padding,
+        'x_right_padding': x_right_padding,
         'sprites_per_row': sprites_per_row, 'sprite_rows': sprite_rows,
-        'row_height': row_height, 'max_height': max_height
+        'row_height': row_height
     }
 
 
@@ -53,11 +54,12 @@ lookup = TemplateLookup(directories=[template_dir])
 template = lookup.get_template('derived.mako')
 # template = Template(filename='derived.mako', lookup=lookup) # for some reason this doesn't work in Azure Functions
 
-unvaccinated = 40
-vaccinated = 3
-date = datetime(2021, 10, 13)
+unvaccinated = 312
+vaccinated = 40
+date = datetime.today()
 
-rendered = template.render(unvaccinated=unvaccinated, vaccinated=vaccinated, date=date, **svg_settings())
+rendered = template.render(unvaccinated=unvaccinated, vaccinated=vaccinated, total_deaths=unvaccinated + vaccinated,
+                           date=date, **svg_settings())
 
 svg_file = './out.svg'
 png_file = './out.png'
